e897ab3fe316600d1efbd69692871dc3c3b80945
aabert <aabert@smail.uni-koeln.de>
2025-04-17 20:03:00 +0200

    implemented Main and Inbox

--- Homework2/src/Excercise3/Inbox.java
+++ Homework2/src/Excercise3/Inbox.java
@@ -1,5 +1,82 @@
 package Excercise3;
 
+import java.util.ArrayList;
+
 public class Inbox {
+	private ArrayList<Mail> inbox;
+
+	// CONSTRUCTORS
+	/**
+	 * parameterless Constructor for Inbox
+	 * 
+	 * @author aabert
+	 */
+	public Inbox() {
+		inbox = new ArrayList<>();
+	}
+
+	/**
+	 * Constructor of Inbox with initilization of Inbox with the Mails stored in the
+	 * mails array
+	 * 
+	 * @param mails the emails to add to the Inbox
+	 */
+	public Inbox(Mail[] mails) {
+		inbox = new ArrayList<>();
+		for (int i = 0; i < mails.length; i++) {
+			inbox.add(mails[i]);
+		}
+	}
+
+	// METHODS
+	/**
+	 * Outprints the header of all emails that are currently in the inbox
+	 * @author aabert
+	 */
+	public void printAllMails() {
+		System.out.println("---Inbox---");
+		int i = 1;
+		for (Mail mail : inbox) {
+			String ind;
+			if (mail.isRead()) {
+				ind = "read";
+			} else {
+				ind = "new";
+			}
+			System.out.println(i + "| " + ind + " | " + mail.getSubject() + " | " + mail.getSenderAdress() + " | "
+					+ mail.getTime().toString());
+			i++;
+		}
+	}
+	
+	/**
+	 * Opens (prints) the email at the specified index (starting at one) from the inbox,
+	 * sets read to true;
+	 * @param index Index of the email to be printed
+	 * @author aabert
+	 */
+	public void open(int index) {
+		if(index> inbox.size() || index<=0) {
+			System.err.println("There is no email with this index in your inbox!");
+			return;
+		}
+		inbox.get(index-1).print();
+		inbox.get(index-1).readMessage();
+	}
+	
+	/**
+	 * counts all unread messages in the inbox and returns the number of those
+	 * @return number of unread messages
+	 * @author aabert
+	 */
+	public int countUnread() {
+		int numberUnread=0;
+		for (Mail mail: inbox) {
+			if(mail.isRead()==false) {
+				numberUnread++;
+			}
+		}
+		return numberUnread;
+	}
 
 }
--- Homework2/src/Excercise3/Main.java
+++ Homework2/src/Excercise3/Main.java
@@ -1,10 +1,31 @@
 package Excercise3;
+import java.util.Scanner;
 
+/**
+ * Testing class
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mail mail1 = new Mail("aabert@smail.uni-koeln.de", "This is a test message.", "Testing");
+		Mail mail2 = new Mail("example@random.com", "This is a useful Email.", "Useful");
+		Mail mail3 = new Mail("person@uni.de", "I want to inform you about this test.", "Test Info");
+		Mail mail4 = new Mail("human@earth.global", "I am writing this message from earth.", "Earth");
+		mail1.print();
+		Mail[] mails = new Mail[4];
+		mails[0]= mail1;
+		mails[1]=mail2;
+		mails[2]=mail3;
+		mails[3]=mail4;
+		Inbox inbox1 = new Inbox(mails);
+		inbox1.printAllMails();
+		System.out.println("Nuber of unread messages: " + inbox1.countUnread());
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the index of the mail to open: ");
+		int input= scanner.nextInt();
+		inbox1.open(input);
+		System.out.println("Nuber of unread messages: " + inbox1.countUnread());
 	}
 
 }
